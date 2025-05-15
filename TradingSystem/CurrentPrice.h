e7b99887843b4bd8e1eb898877f8c5b09b69c79a
jnyoung.choi <wnsdud6992@naver.com>
2025-05-15 14:46:51 +0900

    feature error_

---
+++ TradingSystem/CurrentPrice.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+#include <unordered_map>
+
+static std::unordered_map<std::string, int> currentPrice{
+	{"samsung", 12000},
+	{"kakao", 3200},
+	{"hyndai", 5600},
+	{"hynix", 12000},
+	{"AAA", 6600},
+	{"BBB", 2100},
+	{"CCC", 780},
+};
