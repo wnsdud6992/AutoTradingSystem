ddf0826bb6a361deed9ac6a8fd2e86bf52fea710
DESKTOP-5FBE16T\User <1>
2025-05-15 15:13:33 +0900

    add unittest: sell

--- TradingSystem/AutoTradingSys.cpp
+++ TradingSystem/AutoTradingSys.cpp
@@ -1,11 +1,16 @@
 #include "AutoTradingSys.h"
 
+#include<iostream>
+using namespace std;
+
 AutoTradingSys::AutoTradingSys(IStockerBrockerDriver* driver_, int account) : driver(driver_), userAccount(account)
 {
 }
+
 bool AutoTradingSys::login(std::string id, int password) {
 	return false;
 }
+
 void AutoTradingSys::buy(std::string code, int price, int num) {
 	driver->buy(code, price ,num);
 	if (stockInfo.find(code) != stockInfo.end()) {
@@ -17,15 +22,36 @@ void AutoTradingSys::buy(std::string code, int price, int num) {
 
 	userAccount += price * num;
 }
-void AutoTradingSys::sell(std::string code, int price, int num) {
 
+void AutoTradingSys::sell(std::string code, int price, int num) {
+	userAccount=0;
+
+	auto stock = stockInfo.find(code);
+	if (stock != stockInfo.end()) {
+		// key Á¸ÀçÇÔ
+		int price = stock->second.first;
+		int quantity = stock->second.second;
+		if (quantity < num) {
+			std::cout << "Stock not enough.\n";
+			throw std::exception();
+		}
+		driver->sell(code, price, num);//mock driver sell
+		std::cout << "Price: " << price << ", Quantity: " << quantity << std::endl;
+	}
+	else {
+		std::cout << "Stock not found.\n";
+		throw std::exception();
+	}
 }
+
 int  AutoTradingSys::getPrice(std::string code) {
 	return 0;
 }
+
 void AutoTradingSys::buyNiceTiming(std::string code, int price) {
 
 }
+
 void AutoTradingSys::sellNiceTiming(std::string code, int num) {
 
 }
